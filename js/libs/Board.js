61a70450935841cbeb9ec0038fef59fa37b837de
Amos Wong <amoswwj@gmail.com>
2016-11-17 00:50:33 +0800

    Preserve canvas on resize

--- js/libs/Board.js
+++ js/libs/Board.js
@@ -3,6 +3,8 @@ var Board = (function() {
      resolution: 2,
      dom: null,
      ctx: null,
+     domTemp: null,
+     ctxTemp: null,
      pos: {
        x: 0,
        y: 0
@@ -10,17 +12,45 @@ var Board = (function() {
      init: function init(canvasId) {
        this.dom = document.getElementById(canvasId);
        this.ctx = this.dom.getContext('2d');
+       fitToWindow.bind(this)();
+
        this.pos.x = this.dom.offsetLeft;
        this.pos.y = this.dom.offsetTop;
-
        window.addEventListener('resize', resize.bind(this));
      }
    };
 
-   var resize = function resize(event) {
-     this.pos.x = this.dom.offsetLeft;
-     this.pos.y = this.dom.offsetTop;
-   }
+    var fitToWindow = function fitToWindow() {
+      var margin = 10;
+      var height = window.innerHeight - margin;
+      var height2 = height * this.resolution;
+      var width = window.innerWidth - margin;
+      var width2 = width * this.resolution;
+
+      // Credit: http://stackoverflow.com/questions/5517783/preventing-canvas-clear-when-resizing-window
+      // create a temporary canvas obj to cache the pixel data //
+      var temp_cnvs = document.createElement('canvas');
+      var temp_cntx = temp_cnvs.getContext('2d');
+      // set it to the new width & height and draw the current canvas data into it //
+      temp_cnvs.width = width2;
+      temp_cnvs.height = height2;
+      temp_cntx.fillStyle = '#ffffff';
+      temp_cntx.fillRect(0, 0, width2, height2);
+      temp_cntx.drawImage(this.dom, 0, 0);
+      // resize & clear the original canvas and copy back in the cached pixel data //
+      this.dom.style.height = height + 'px';
+      this.dom.style.width = width + 'px';
+      this.dom.width = width2;
+      this.dom.height = height2;
+      this.ctx.drawImage(temp_cnvs, 0, 0);
+    }
+
+    var resize = function resize(event) {
+      fitToWindow.bind(this)();
+
+      this.pos.x = this.dom.offsetLeft;
+      this.pos.y = this.dom.offsetTop;
+    }
 
    return boardObject;
 })();
