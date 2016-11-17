8ecb70c9ed20f5d5200e28d26502e36b0db8d99a
amoshydra <amoswwj@gmail.com>
2016-11-17 13:29:12 +0800

    Add buffer to retain out of window canvas

--- js/libs/Board.js
+++ js/libs/Board.js
@@ -3,8 +3,8 @@ var Board = (function() {
      resolution: 2,
      dom: null,
      ctx: null,
-     domTemp: null,
-     ctxTemp: null,
+     domMem: null,
+     ctxMem: null,
      pos: {
        x: 0,
        y: 0
@@ -12,6 +12,10 @@ var Board = (function() {
      init: function init(canvasId) {
        this.dom = document.getElementById(canvasId);
        this.ctx = this.dom.getContext('2d');
+
+       // Create buffer
+       this.domMem = document.createElement('canvas');
+       this.ctxMem = this.domMem.getContext('2d');
 
        // Set up sizing
        fitToWindow.bind(this)();
@@ -30,27 +34,42 @@ var Board = (function() {
 
     var fitToWindow = function fitToWindow() {
       var margin = 10;
-      var height = window.innerHeight - margin;
-      var height2 = height * this.resolution;
-      var width = window.innerWidth - margin;
-      var width2 = width * this.resolution;
 
-      // Credit: http://stackoverflow.com/questions/5517783/preventing-canvas-clear-when-resizing-window
-      // create a temporary canvas obj to cache the pixel data //
-      var temp_cnvs = document.createElement('canvas');
-      var temp_cntx = temp_cnvs.getContext('2d');
-      // set it to the new width & height and draw the current canvas data into it //
-      temp_cnvs.width = width2;
-      temp_cnvs.height = height2;
-      temp_cntx.fillStyle = '#ffffff';
-      temp_cntx.fillRect(0, 0, width2, height2);
-      temp_cntx.drawImage(this.dom, 0, 0);
-      // resize & clear the original canvas and copy back in the cached pixel data //
-      this.dom.style.height = height + 'px';
-      this.dom.style.width = width + 'px';
-      this.dom.width = width2;
-      this.dom.height = height2;
-      this.ctx.drawImage(temp_cnvs, 0, 0);
+      var heightCss = window.innerHeight - margin;
+      var heightCanvas = height * this.resolution;
+      var widthCss = window.innerWidth - margin;
+      var widthCanvas = width * this.resolution;
+
+      // If new size is larger than memory
+      if (widthCanvas > this.domMem.width || heightCanvas > this.domMem.height) {
+        // Create buffer
+        var bufferCanvas = document.createElement('canvas');
+        var bufferCtx = bufferCanvas.getContext('2d');
+
+        bufferCanvas.width = widthCanvas;
+        bufferCanvas.height = heightCanvas;
+
+        // Clear buffer
+        bufferCtx.fillStyle = '#ffffff';
+        bufferCtx.fillRect(0, 0, widthCanvas, heightCanvas);
+
+        // Save canvas to buffer
+        bufferCtx.drawImage(this.dom, 0, 0);
+
+        // Resize memory
+        this.domMem.width = widthCanvas;
+        this.domMem.height = heightCanvas;
+        this.ctxMem.drawImage(bufferCanvas, 0, 0);
+      } else {
+        this.ctxMem.drawImage(this.dom, 0 ,0);
+      }
+
+      // resize current canvas
+      this.dom.style.height = heightCss + 'px';
+      this.dom.style.width = widthCss + 'px';
+      this.dom.width = widthCanvas;
+      this.dom.height = heightCanvas;
+      this.ctx.drawImage(this.domMem, 0, 0);
 
       this.pos.x = this.dom.offsetLeft;
       this.pos.y = this.dom.offsetTop;
