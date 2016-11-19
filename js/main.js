b8dcad0ea4ee2769b4ae1c440c50d9a050d4a4b6
amoshydra <amoswwj@gmail.com>
2016-11-20 00:50:51 +0800

    Fix canvas not being drawn when a finger is released

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ var pointerHash = {};
 
 // Attach event listener
 var leaveBoard = function leaveBoard(e) {
-  Pen.release();
-  delete pointerHash[e.pointerId];
+  if (pointerHash[e.pointerId]) {
+    delete pointerHash[e.pointerId];
+  }
 };
 
 Board.dom.addEventListener('pointerdown', function(e) {
@@ -34,19 +35,21 @@ Board.dom.addEventListener('pointerleave', leaveBoard);
 
 // Draw method
 function drawOnCanvas(e, pointerObj, Pen) {
-  pointerObj.set(Board.getPointerPos(e));
-  Pen.setPen(Board.ctx, e);
+  if (pointerObj) {
+    pointerObj.set(Board.getPointerPos(e));
+    Pen.setPen(Board.ctx, e);
 
-  if (pointerObj.pos0.x < 0) {
-    pointerObj.pos0.x = pointerObj.pos1.x - 1;
-    pointerObj.pos0.y = pointerObj.pos1.y - 1;
+    if (pointerObj.pos0.x < 0) {
+      pointerObj.pos0.x = pointerObj.pos1.x - 1;
+      pointerObj.pos0.y = pointerObj.pos1.y - 1;
+    }
+    Board.ctx.beginPath();
+    Board.ctx.moveTo(pointerObj.pos0.x, pointerObj.pos0.y)
+    Board.ctx.lineTo(pointerObj.pos1.x, pointerObj.pos1.y);
+    Board.ctx.closePath();
+    Board.ctx.stroke();
+
+    pointerObj.pos0.x = pointerObj.pos1.x;
+    pointerObj.pos0.y = pointerObj.pos1.y;
   }
-  Board.ctx.beginPath();
-  Board.ctx.moveTo(pointerObj.pos0.x, pointerObj.pos0.y)
-  Board.ctx.lineTo(pointerObj.pos1.x, pointerObj.pos1.y);
-  Board.ctx.closePath();
-  Board.ctx.stroke();
-
-  pointerObj.pos0.x = pointerObj.pos1.x;
-  pointerObj.pos0.y = pointerObj.pos1.y;
 }
